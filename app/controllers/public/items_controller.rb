d160b5d7e7d367eb2fdb3119079b5f80f33eba2f
hare145 <130465393+hare145@users.noreply.github.com>
2023-06-30 17:15:40 +0000

    wbs3.9.2

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,11 +2,15 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
     @item_count = Item.count
+    @genres = Genre.all
+    @item_genre = Item.where(genre_id: params[:genre_id])
+    @item_genre_count = Item.where(genre_id: params[:genre_id]).count
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
   
 end
