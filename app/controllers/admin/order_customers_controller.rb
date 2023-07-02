ce9ec0ce3903990b39b20ed9b419278ab93337c4
hare145 <130465393+hare145@users.noreply.github.com>
2023-07-02 14:08:12 +0000

    wbs3.9.2

--- app/controllers/admin/order_customers_controller.rb
+++ app/controllers/admin/order_customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrderCustomersController < ApplicationController
   def show
     @order_customer = Order.find_by(customer_id: params[:id])
-    @order = Order.where(customer: params[:id])
+    @order = Order.where(customer: params[:id]).page(params[:page]).per(10)
   end
 end
